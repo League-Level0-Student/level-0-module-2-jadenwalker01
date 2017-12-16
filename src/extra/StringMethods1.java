d8383ba3d5605fd0e9c484aa4b837c13dea527e0
jadenwalker01 <league@WorthyMini-6.attlocal.net>
2017-12-16 14:28:14 -0800

    intro to strings part 1&2

--- src/extra/StringMethods1.java
+++ src/extra/StringMethods1.java
@@ -2,28 +2,33 @@
 //    Level 0
 package extra;
 
+import javax.swing.JOptionPane;
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+		String loop = "code4life"; 
 		// 1. Create a String variable and initialize it to whatever you want
 
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
 
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+//		for (int i = 0; i < loop.length(); i++) {
+//			System.out.println(loop.charAt(i) );
+//		}
+		
+		System.out.println(loop.charAt(0));
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+
 	}
 }
 
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -2,24 +2,29 @@
 //    Level 0
 package extra;
 
+import java.util.Random;
+
 public class StringMethods2 {
 	public static void main(String[] args) {
-		
+		int random = new Random().nextInt(8);	
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+String loop = "JjGHgfvF";
 		// 2. Print your String to the console in upper case.
-		
+	//	System.out.println(loop.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+//		System.out.println(loop.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
-		// 5. Print a single char somewhere in the middle of your String
+	//	System.out.println(loop.substring(0,3));
+		// 5. Print a RANDOM single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+//		System.out.println(loop.charAt(random)); 
+System.out.println(loop.substring(loop.length()-3, loop.length()));
+
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		
 	}
 }
 
